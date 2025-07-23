99c51ed01c9f47d53b75a7fde60c513523941fdd
Trikaal212 <anikets993@gmail.com>
2025-07-23 20:07:53 +0530

    roting change

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Faq from './pages/Faq'
-import Pricing from './pages/Pricing'
-import GlobalLayout from './layouts/GlobalLayout'
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Faq from './pages/Faq';
+import Pricing from './pages/Pricing';
+import GlobalLayout from './layouts/GlobalLayout';
 
 const Routers = () => {
   return (
-    <BrowserRouter  basename="/thefront">
-
-    <Routes>
-        <Route path='/' element={<GlobalLayout/>}  >
-        <Route path='' element={<Home/>}/>
-        <Route path='about' element={<About/>}/>
-        <Route path='contact' element={<Contact/>}/>
-        <Route path='faq' element={<Faq/>}/>
-        <Route path='pricing' element={<Pricing/>}/>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<GlobalLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="faq" element={<Faq />} />
+          <Route path="pricing" element={<Pricing />} />
         </Route>
-    </Routes>
-    
-    </BrowserRouter>
-  )
-}
+      </Routes>
+    </HashRouter>
+  );
+};
 
-export default Routers
+export default Routers;
